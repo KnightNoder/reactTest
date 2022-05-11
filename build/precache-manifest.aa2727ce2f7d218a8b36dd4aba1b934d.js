3047803bc18e962be751fac524c5d7ef67cef465
lava <lavakumared15@gmail.com>
2022-05-11 15:02:54 +0530

    dummy commit for main.js fix

---
+++ build/precache-manifest.aa2727ce2f7d218a8b36dd4aba1b934d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7114beacbb210408ee54",
+    "url": "/static/css/main.43255e99.chunk.css"
+  },
+  {
+    "revision": "7114beacbb210408ee54",
+    "url": "/static/js/main.7114beac.chunk.js"
+  },
+  {
+    "revision": "245363e5d9e0f292cf27",
+    "url": "/static/js/1.245363e5.chunk.js"
+  },
+  {
+    "revision": "3a3f5e5bd105de6af0a9",
+    "url": "/static/css/2.3b75e197.chunk.css"
+  },
+  {
+    "revision": "3a3f5e5bd105de6af0a9",
+    "url": "/static/js/2.3a3f5e5b.chunk.js"
+  },
+  {
+    "revision": "0595fa594ad034a46c49",
+    "url": "/static/js/runtime~main.0595fa59.js"
+  },
+  {
+    "revision": "0e4e4b581a6cf00feb7330c2dbb9687a",
+    "url": "/static/media/Menu.0e4e4b58.jpg"
+  },
+  {
+    "revision": "8fdc5d25eda4dbeac50273fc972f116c",
+    "url": "/static/media/gift2.8fdc5d25.png"
+  },
+  {
+    "revision": "f083443817b919b91a8aaff246117f41",
+    "url": "/static/media/rewardsImage.f0834438.png"
+  },
+  {
+    "revision": "0b590c70036842b37da3824d7161b3da",
+    "url": "/static/media/how.0b590c70.jpg"
+  },
+  {
+    "revision": "c9f77fcf35dacc09c69cd0e0d7e10102",
+    "url": "/static/media/WalletCardImage.c9f77fcf.jpg"
+  },
+  {
+    "revision": "e58dc30039cd0f0497a3b5be5e81c2eb",
+    "url": "/static/media/HarmoniaSansProCyr-Regular.e58dc300.ttf"
+  },
+  {
+    "revision": "ec79aeaf67ab8aa5c3c81f4d6bd30910",
+    "url": "/static/media/HarmoniaSansProCyr-Bold.ec79aeaf.ttf"
+  },
+  {
+    "revision": "a862c60eadc6ebe6dc2179a90e95dbbe",
+    "url": "/index.html"
+  }
+];
